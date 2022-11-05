89b444fc5960be0730464ad73e89df3ee2698cf1
Oscar Mesh <okerekeifeanyi65@gmail.com>
2022-11-05 14:26:45 +0100

    second commit

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,20 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+// import Repos from "../../pages/Repos/Repos";
 import "./Card.css";
 
-const Card = () => {
+const Card = ({ repos }) => {
   return (
-    <div className="card">
-      <div className="head">
-        <img src="" alt="" />
-        <span>
-          <h3>OscarMesh</h3>
-          <p>Title of Repo</p>
-        </span>
-      </div>
-      <p>The repository was created on 20th June 2022 by OscarMesh </p>
-
-      <button>View Repo</button>
-    </div>
+    <>
+      {repos.map((item) => {
+        return (
+          <div className="card" key={item.id}>
+            <div className="head">
+              <img
+                src="https://github.githubassets.com/images/modules/profile/achievements/pull-shark-default.png"
+                alt="oscarmesh"
+              />
+              <span>
+                <h3>OscarMesh</h3>
+                <p>{item.name}</p>
+              </span>
+            </div>
+            <p>The repository was created on 20th June 2022 by OscarMesh </p>
+            <Link to="/">
+              <span>View Repo</span>
+            </Link>
+          </div>
+        );
+      })}
+    </>
   );
 };
 
--- src/pages/Repos/Repos.js
+++ src/pages/Repos/Repos.js
@@ -1,10 +1,92 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Repos.css";
 import { Icon } from "@iconify/react";
+import axios from "axios";
 import SideBar from "../../components/SideBar/Sidebar";
 import Card from "../../components/Card/Card";
 
 const Repos = () => {
+  const [repos, setRepos] = useState([]);
+  const [currentPage, setcurrentPage] = useState(1);
+  const [itemsPerPage, setitemsPerPage] = useState(4);
+  const [pageNumberLimit, setpageNumberLimit] = useState(4);
+  const [maxPageNumberLimit, setmaxPageNumberLimit] = useState(4);
+  const [minPageNumberLimit, setminPageNumberLimit] = useState(0);
+
+  const handleClick = (event) => {
+    setcurrentPage(Number(event.target.id));
+  };
+
+  const pages = [];
+  for (let i = 1; i <= Math.ceil(repos.length / itemsPerPage); i++) {
+    pages.push(i);
+  }
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = repos.slice(indexOfFirstItem, indexOfLastItem);
+
+  const renderPageNumbers = pages.map((number) => {
+    if (number < maxPageNumberLimit + 1 && number > minPageNumberLimit) {
+      return (
+        <li
+          key={number}
+          id={number}
+          onClick={handleClick}
+          className={currentPage == number ? "active" : null}
+        >
+          {number}
+        </li>
+      );
+    } else {
+      return null;
+    }
+  });
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios.get(
+        "https://api.github.com/users/OscarMesh/repos"
+      );
+      console.log(data);
+      setRepos(data);
+    };
+
+    getData();
+  }, []);
+
+  const handleNextbtn = () => {
+    setcurrentPage(currentPage + 1);
+
+    if (currentPage + 1 > maxPageNumberLimit) {
+      setmaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
+      setminPageNumberLimit(minPageNumberLimit + pageNumberLimit);
+    }
+  };
+
+  const handlePrevbtn = () => {
+    setcurrentPage(currentPage - 1);
+
+    if ((currentPage - 1) % pageNumberLimit == 0) {
+      setmaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
+      setminPageNumberLimit(minPageNumberLimit - pageNumberLimit);
+    }
+  };
+
+  let pageIncrementBtn = null;
+  if (pages.length > maxPageNumberLimit) {
+    pageIncrementBtn = <li onClick={handleNextbtn}> &hellip; </li>;
+  }
+
+  let pageDecrementBtn = null;
+  if (minPageNumberLimit >= 1) {
+    pageDecrementBtn = <li onClick={handlePrevbtn}> &hellip; </li>;
+  }
+
+  // const handleLoadMore = () => {
+  //   setitemsPerPage(itemsPerPage + 5);
+  // };
+
   return (
     <div className="repos">
       <SideBar />
@@ -21,8 +103,33 @@ const Repos = () => {
         </div>
 
         <div className="repoList">
-          <Card />
+          <Card repos={currentItems} />
         </div>
+        <ul className="pageNumbers">
+          <li>
+            <span
+              onClick={handlePrevbtn}
+              disabled={currentPage == pages[0] ? true : false}
+            >
+              Prev
+            </span>
+          </li>
+          {pageDecrementBtn}
+          {renderPageNumbers}
+          {pageIncrementBtn}
+
+          <li>
+            <span
+              onClick={handleNextbtn}
+              disabled={currentPage == pages[pages.length - 1] ? true : false}
+            >
+              Next
+            </span>
+          </li>
+        </ul>
+        {/* <button onClick={handleLoadMore} className="loadmore">
+          Load More
+        </button> */}
       </div>
     </div>
   );
