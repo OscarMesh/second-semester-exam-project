433c0cf13a149d8be684a1bc691c1aa010661d0c
Oscar Mesh <okerekeifeanyi65@gmail.com>
2022-11-06 20:05:54 +0100

    second commit

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -3,6 +3,7 @@ import "./ErrorPage.css";
 import { Link } from "react-router-dom";
 import error from "../../assets/404.json";
 import Lottie from "react-lottie";
+import { Helmet } from "react-helmet-async";
 
 const ErrorPage = () => {
   const defaultOptions = {
@@ -14,14 +15,21 @@ const ErrorPage = () => {
     },
   };
   return (
-    <div className="error">
-      <div className="error-page">
-        <Lottie options={defaultOptions} />
-        <Link to="/">
-          <button>Home Page</button>
-        </Link>
+    <>
+      <Helmet>
+        <title>Error Page</title>
+        <meta name="description" content="Page not found" />
+        <link rel="canonical" href="*" />
+      </Helmet>
+      <div className="error">
+        <div className="error-page">
+          <Lottie options={defaultOptions} />
+          <Link to="/">
+            <button>Home Page</button>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
